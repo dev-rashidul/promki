4cd75f1a913826a06db2eaab4a3b82351e92ca7f
Rashidul Islam <rashidul.contact@gmail.com>
2024-05-04 12:26:15 +0600

    Product Page done

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,8 @@ $(".product-slider").owlCarousel({
   },
 });
 
+// Product Details Slider
+
 $(".product-details-slider").owlCarousel({
   loop: true,
   nav: true,
@@ -36,3 +38,50 @@ $(".product-details-slider").owlCarousel({
     '<i class="fa-solid fa-arrow-right-long"></i>',
   ],
 });
+
+
+// Price History Chart
+
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: ["16 Wrz 23", "16 Lis 23", "16 Sty 24", "16 Mar 24"],
+    datasets: [
+      {
+        label: "Price History",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+
+// Expand Desctiption
+
+const exdBtn = document.getElementById("exdBtn")
+const productDescription = document.getElementById("productDescription")
+
+exdBtn.addEventListener("click", ()=>{
+  productDescription.classList.toggle("active")
+  exdBtn.style.display = "none"
+})
+
+// Expand Technical Data
+
+const exTdBtn = document.getElementById("exTdBtn")
+const tdCard = document.getElementById("tdCard")
+
+exTdBtn.addEventListener("click", ()=>{
+  tdCard.classList.toggle("active")
+  exTdBtn.style.display = "none"
+})
